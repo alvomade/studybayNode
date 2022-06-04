22a456474fd455aaefca1c715e6254ed2ec5faa0
alvomade <malvinokeyo@gmail.com>
2022-06-04 05:25:24 +0300

    added a thread sleep

--- messenger.js
+++ messenger.js
@@ -2,6 +2,7 @@ const {parentPort,workerData}=require('worker_threads')
 const { Builder, By, Key, until, WebElement, Capabilities, WebDriver } = require('selenium-webdriver');
 require('chromedriver');
 const chrome = require("selenium-webdriver/chrome");
+const sleep = require('sleep');
 var options = new chrome.Options();
 // options.addArguments("--headless");
 // options.addArguments("dissable-gpu")
@@ -26,12 +27,7 @@ options.addArguments("--log-level=3");
     let orders=null
     let odaID=null
     while(1>0){
-        // await driver.manage().setTimeouts( { implicit: 10000 } );
-
-            // setTimeout(async()=>{
-            //     console.log('WAITING KIASI')
-            // },50000)
-
+    
         try{
             orders=await driver.wait(until.elementsLocated(By.xpath("//div[@class='countMessages orders__item_bottom__messages']")),30000)
 
@@ -60,7 +56,7 @@ options.addArguments("--log-level=3");
 
             //check if message was already sent
             try{
-                await driver.wait(until.elementLocated(By.xpath("//div[@class='im__content_message-text '] ")),10000)
+                await driver.wait(until.elementLocated(By.xpath("//div[@class='im__content_message-text '] ")),5000)
                 console.log('KUNA MESSAGE ALREADY')
                 // await driver.executeScript('arguments[0].click()',oda)
                 workerData.alreadyMessaged.push(odaID)
@@ -70,7 +66,7 @@ options.addArguments("--log-level=3");
                 break
 
             }catch(err){
-                console.log('WRITING MESSAGE...................',err)
+                console.log('WRITING MESSAGE...................')
                 try{
                     await driver.wait(until.elementLocated(By.id(`comment`)),15000).sendKeys(workerData.secondMessage)
                     let sendBtn=await driver.wait(until.elementLocated(By.id(`control_submitter`)),5000)
@@ -88,6 +84,7 @@ options.addArguments("--log-level=3");
         }
 
         await driver.get("https://studybay.app/home/myorders?type=0")
+        sleep.sleep(5)
         // setTimeout(async()=>{
         //     console.log('REFRESH AFTER 10 SECONDS')
         //     await driver.get("https://studybay.app/home/myorders?type=0")
